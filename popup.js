85d9d16c6c0efbbfdd0781279432ddfee44ed082
vikrantaroraa <arora.veshu@gmail.com>
2025-10-23 19:45:25 +0530

    refactor: moved fn to send message to background inside click fn

--- popup.js
+++ popup.js
@@ -2,11 +2,11 @@ console.log("Popup script loaded");
 
 document.getElementById("send").addEventListener("click", () => {
   console.log("sending message to background");
-});
 
-// send message to background
-chrome.runtime.sendMessage({ from: "popup", action: "GREET" }, (response) => {
-  console.log("Got reply from background:- ", response);
-  // Optionally show it to the user
-  alert(`Popup got reply: ${response.reply}`);
+  // send message to background
+  chrome.runtime.sendMessage({ from: "popup", action: "GREET" }, (response) => {
+    console.log("Got reply from background:- ", response);
+    // Optionally show it to the user
+    alert(`Popup got reply: ${response.reply}`);
+  });
 });
